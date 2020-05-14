dafb04d0043d6a4210110dc273107da119ce217a
Hugo Leclère <leclerehugo1@gmail.com>
2020-05-14 10:48:54 +0200

    change exception message

--- FabricObserver/Observers/Utilities/AzureQueueObserverAccessor.cs
+++ FabricObserver/Observers/Utilities/AzureQueueObserverAccessor.cs
@@ -114,7 +114,7 @@ namespace FabricObserver.Observers.Utilities
         {
             if (!IsOpenQueueCalled)
             {
-                throw new Exception("CloudQueue was initialized.");               
+                throw new Exception("CloudQueue has to be initialized.");               
             }
         }
     }
