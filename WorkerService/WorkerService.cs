9c48dba070f349aaf6c9870459e53f44a776a13c
Hugo Leclère <leclerehugo1@gmail.com>
2020-05-23 17:22:48 +0200

    adding service remoting

--- WorkerService/WorkerService.cs
+++ WorkerService/WorkerService.cs
@@ -7,13 +7,21 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 
 namespace WorkerService
 {
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
     /// </summary>
-    internal sealed class WorkerService : StatelessService
+
+    public interface IWorkerService : IService
+    {
+        Task<string> SendHeartbeat(CancellationToken cancellationToken);
+    }
+
+    internal sealed class WorkerService : StatelessService, IWorkerService
     {
         private readonly TimeSpan TimeInterval = TimeSpan.FromSeconds(10);
 
@@ -29,14 +37,14 @@ namespace WorkerService
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
-            return new ServiceInstanceListener[0];
+            return this.CreateServiceRemotingInstanceListeners();
         }
 
         /// <summary>
         /// This is the main entry point for your service instance.
         /// </summary>
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
-        protected override async Task RunAsync(CancellationToken cancellationToken)
+        public Task<string> SendHeartbeat(CancellationToken cancellationToken)
         {
             string senderId = this.Context.CodePackageActivationContext.ApplicationName + this.Context.InstanceId + this.Context.PartitionId.ToString();
             Random random = new Random();
@@ -47,9 +55,8 @@ namespace WorkerService
                 string timestamp = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
                 int workerStatusLength = Enum.GetNames(typeof(WorkerStatus)).Length;
                 WorkerStatus status = (WorkerStatus) random.Next(workerStatusLength);
-
-                ServiceEventSource.Current.ServiceMessage(this.Context, $"{senderId}/{timestamp}/{status}");
-                await Task.Delay(TimeInterval, cancellationToken);
+                Task.Delay(TimeInterval, cancellationToken);
+                return Task.FromResult($"{senderId}/{timestamp}/{status}");
             }
         }
     }
