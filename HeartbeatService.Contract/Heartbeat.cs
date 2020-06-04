7c19d16af99a6e7653df71f47088092003605312
Hugo Leclère <leclerehugo1@gmail.com>
2020-06-04 15:19:55 +0200

    adding dataContract / private setter

--- HeartbeatService.Contract/Heartbeat.cs
+++ HeartbeatService.Contract/Heartbeat.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HeartbeatService.Contract
 {
+    [DataContract]
     public class Heartbeat
     {
-        public string SenderId { get; }
-        public DateTime Timestamp { get; }
-        public string Status { get; }
-
+        [DataMember]
+        public string SenderId { get; private set; }
+        [DataMember]
+        public DateTime Timestamp { get; private set; }
+        [DataMember]
+        public string Status { get; private set; }
+        public Heartbeat(){}
         public Heartbeat(string senderId, DateTime timestamp, string status)
         {
             this.SenderId = senderId;
